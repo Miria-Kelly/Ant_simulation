aa9e5755f00980f6ef4d9ea1fc69d1dc495c6d0d
Miria-Kelly <miriakelly.14@gmail.com>
2026-07-30 11:24:03 -0300

    commitando

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.*;
+
+/*GL_STATIC_DRAW → os dados quase nunca mudam (ex.: uma árvore, uma pedra, uma casa).
+GL_DYNAMIC_DRAW → os dados mudam de vez em quando.
+GL_STREAM_DRAW → os dados mudam praticamente a cada frame.
+*/
+
+public class Main {
+
+}
